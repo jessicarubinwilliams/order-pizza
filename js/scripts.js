bb96c96ae14772c13ccd4f8ad3648ee96b9a8236
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-01-29 14:00:24 -0800

    Add functionality for outputing cost with the order confirmation.

--- js/scripts.js
+++ js/scripts.js
@@ -3,12 +3,14 @@
 $(document).ready(function() {
   $("form#orderForm").submit(function(event) {
     event.preventDefault();
-    const sizeSelection = $("#size").val();
-    $("#pizzaSize").text(sizeSelection);
     $("input:checkbox[name=topping]:checked").each(function() {
       const toppingSelection = $(this).val(); //this refers to the specific selector being iterated over
-      $("div#output").append(toppingSelection + "<br>");
+      $("div#confirmation").append(toppingSelection + "<br>");
     });
+    const sizeSelection = $("#size").val();
+    $("#pizzaSize").text(sizeSelection);
+    const totalDue = ("6") //change value one Business Logic is developed
+    $("#price").append(totalDue);
     $("div#output").show();
     $("form#orderForm").hide();
   });
