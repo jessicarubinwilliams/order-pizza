c630ba69b0029d3a8548c511aadf33a832537971
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-01-29 13:51:07 -0800

    Add functionality for pizza size selection and output of the size selection in order confirmation.

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,13 @@
 $(document).ready(function() {
   $("form#orderForm").submit(function(event) {
     event.preventDefault();
-    $("div#output").show();
+    const sizeSelection = $("#size").val();
+    $("#pizzaSize").text(sizeSelection);
     $("input:checkbox[name=topping]:checked").each(function() {
-      const toppingSelection = $(this).val();
+      const toppingSelection = $(this).val(); //this refers to the specific selector being iterated over
       $("div#output").append(toppingSelection + "<br>");
     });
+    $("div#output").show();
     $("form#orderForm").hide();
   });
 });
